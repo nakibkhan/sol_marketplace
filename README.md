d369a418a71e7b78973381014915c26321ceb801
Nakib A. Khan <nakib.khan1988@gmail.com>
2021-06-16 23:03:00 -0500

    (add) initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# sol_marketplace
-Blockchain Marketplace example using Solidity and React
+# Getting started with Solidity Eth App
+
+This is a sample project setup for a React client app communicating with backend Block Chain smart contract
---
+++ server/truffle-config.js
@@ -0,0 +1,54 @@
+
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  contracts_directory: './contracts',
+  contracts_build_directory: './abis'
+  // Configure your compilers
+  compilers: {
+    solc: {
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
